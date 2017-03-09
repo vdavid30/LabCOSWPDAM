eb4ff1f44766d75fca3a7e885ec4fd6b19f7f56c
Santiago Carrillo <sancarbar@gmail.com>
2017-03-09 17:00:50 -0500

    Update TeamDao.

--- app/src/main/java/co/edu/pdam/eci/persistenceapiintegration/data/dao/TeamDao.java
+++ app/src/main/java/co/edu/pdam/eci/persistenceapiintegration/data/dao/TeamDao.java
@@ -1,9 +1,12 @@
 package co.edu.pdam.eci.persistenceapiintegration.data.dao;
 
+import co.edu.pdam.eci.persistenceapiintegration.data.entity.Team;
+
 /**
  * @author Santiago Carrillo
  */
 
 public interface TeamDao
+    extends BaseDao<Team, Long>
 {
 }
