a44bb5631478debbfd555a0c30063c0a0345ec5f
unknown <2114928@labinfo.is.escuelaing.edu.co>
2018-11-07 18:04:48 -0500

    Avance

--- app/src/main/java/co/edu/pdam/eci/persistenceapiintegration/data/dao/TeamDao.java
+++ app/src/main/java/co/edu/pdam/eci/persistenceapiintegration/data/dao/TeamDao.java
@@ -1,12 +1,34 @@
 package co.edu.pdam.eci.persistenceapiintegration.data.dao;
 
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
 import co.edu.pdam.eci.persistenceapiintegration.data.entity.Team;
 
 /**
  * @author Santiago Carrillo
  */
+@Dao
+public interface TeamDao extends BaseDao<Team, Long> {
+    @Query("SELECT * FROM teams")
+    List<Team> getAll();
 
-public interface TeamDao
-    extends BaseDao<Team, Long>
-{
+    @Query("SELECT * FROM teams WHERE uid IN (:teamIds)")
+    List<Team> loadAllByIds(int[] teamIds);
+
+    @Query("SELECT * FROM teams WHERE id LIKE :id AND " +
+            "team_name LIKE :name LIMIT 1")
+    Team findByName(Long id, String name);
+
+    @Insert
+    void insertAll(Team... users);
+
+    @Delete
+    void delete(Team user);
+
+
 }
--- app/src/main/java/co/edu/pdam/eci/persistenceapiintegration/data/entity/Team.java
+++ app/src/main/java/co/edu/pdam/eci/persistenceapiintegration/data/entity/Team.java
@@ -1,11 +1,49 @@
 package co.edu.pdam.eci.persistenceapiintegration.data.entity;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
 /**
  * @author Santiago Carrillo
  */
 
 //TODO add database annotations and proper model structure
-public class Team
-    extends BaseEntity
-{
+@Entity
+public class Team extends BaseEntity {
+
+    @PrimaryKey
+    private Long id;
+    @ColumnInfo(name= "name")
+    private String name;
+    @ColumnInfo(name= "short_name")
+    private String shortName;
+    public Team(){}
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
 }
